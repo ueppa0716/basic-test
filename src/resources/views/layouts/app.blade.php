89bde4680e5faebd8e4c2c2daa70d679c37f10dc
ueppa <ueppa0716@gmail.com>
2024-03-22 22:30:05 +0900

    3/22アップロード分

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atte</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/common.css')}}">
+    @yield('css')
+</head>
+
+<body>
+    <div class="app">
+        <header class="header">
+            <h1 class="header__heading">Atte</h1>
+            @yield('link')
+        </header>
+        <div class="content">
+            @yield('content')
+        </div>
+    </div>
+</body>
+
+</html>
